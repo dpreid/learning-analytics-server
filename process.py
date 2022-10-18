d5b3e1e6b4f0e850af1fe44d53596ad3f1ea8e7a
dpreid <dprydereid@gmail.com>
2022-10-18 18:24:50 +0100

    generating adjacency matrix will update previous file

--- process.py
+++ process.py
@@ -15,6 +15,13 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 import json
+import os
+import math
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+
+data_dir = "./test/data"
+
 """
 Takes a new log message and adds it to the appropriate user log file
 message is already in json format and been loaded into json previously
@@ -25,7 +32,7 @@ def AddUserLog(message):
         exp = message["exp"]
         filename = '%s-%s.json' % (user, exp)
 
-        with open('/app/' + filename, 'a') as outfile:
+        with open('%s/%s' % (data_dir, filename), 'a') as outfile:
             d = json.dumps(message)
             outfile.write(d)
             outfile.write('\n')
@@ -35,18 +42,61 @@ def AddUserLog(message):
 
 """
 Creates a visual representation of a user graph from a user adjacency matrix
+
+Returns a graph representing student data or an empty graph if no data is present.
 """
 def GenerateGraph(user, exp):
     
-    return
+    if(os.path.isfile('%s/%s-%s-adjacency.csv' % (data_dir, user, exp))):
+        df = pd.read_csv('%s/%s-%s-adjacency.csv' % (data_dir, user, exp), index_col=0)
+        G = nx.from_pandas_adjacency(df, create_using=nx.DiGraph)
+    else:
+        G = nx.DiGraph()
+
+    G = PositionGraphNodes(G, 'spinner')
+    
+    return G
+
+
+def DrawGraph(user, exp):
+    
+    G = GenerateGraph(user, exp)
+    g=Network(height=800,width=800,notebook=False, directed=True)
+    g.from_nx(G)
+    g.set_options("""
+        var options = {
+    "nodes": {
+        "font": {
+        "color": "rgba(255,255,255,1)"
+        }
+    },
+    "edges": {
+        "color": {
+        "inherit": true
+        },
+        "smooth": false
+    },
+    "physics": {
+        "minVelocity": 0.75
+    }
+    }
+    """)
+    ## g.show_buttons()
+    g.save_graph('%s/%s-%s-graph.png' % (data_dir, user, exp))
+
+    # nx.draw(G, )
+    # plt.savefig('%s/%s-%s-graph.png' % (data_dir, user, exp))
+    # plt.show()
 
 
 """
 Converts a user log file into a adjacency matrix
 
 If the matrix already exists, then it adds the log file to that matrix.
+
+Once the matrix is updated it will delete the log file
 """
-def GenerateAdjacencyMatrix(user, exp):
+def GenerateAdjacencyMatrix(user, exp, deleteLogFile = True):
     command_array = GetCommandList(user, exp)
 
     if(exp == "spinner"):
@@ -54,32 +104,68 @@ def GenerateAdjacencyMatrix(user, exp):
     else:
         nodes = []
 
-    zero = np.zeros(shape=(len(nodes), len(nodes)))
-    df = pd.DataFrame(zero, nodes, nodes)
+    # if an adjacency matrix for a user already exists, then add new data to that
+    if(os.path.isfile('%s/%s-%s-adjacency.csv' % (data_dir, user, exp))):
+        df = pd.read_csv('%s/%s-%s-adjacency.csv' % (data_dir, user, exp), index_col=0)
+    else:
+        matrix = np.zeros(shape=(len(nodes), len(nodes)))
+        df = pd.DataFrame(matrix, nodes, nodes)
 
-    ## add additional weight to 
+    ## add additional weight to the adjacency matrix
     for index, command in enumerate(command_array):
         if(index + 1 < len(command_array)):
             df[command][command_array[index+1]] += 1
 
-    df.to_csv('%s-%s-adjacency.csv' % (user, exp))
-    return
+    # output the updated csv to file
+    df.to_csv('%s/%s-%s-adjacency.csv' % (data_dir, user, exp))
+
+    # remove the user log file
+    if(deleteLogFile):
+        os.remove('%s/%s-%s.json' % (data_dir, user, exp))
+
+
 
 """
 Generates a list of student commands from the user log file
 Returns and array of commands sent to hardware
 """
 def GetCommandList(user, exp):
-    return []
+    command_array = []
 
-"""
-Creates the base graph for the spinner experiment.
-"""
-def CreateEmptySpinnerGraph():
-    return 
+    with open('%s/%s-%s.json' % (data_dir, user, exp)) as f:
+        lines = f.readlines()
+        for line in lines:
+            if len(line)>1:
+                try:
+                    log_data = json.loads(line)
+                    #command sent
+                    if(log_data["payload"]["log"] == 'voltage'):
+                        command_array.append('voltage_step')
+                    elif(log_data["payload"]["log"] == 'voltage_ramp'):
+                        command_array.append('voltage_ramp')
+                    elif(log_data["payload"]["log"] == 'position'):
+                        command_array.append('position_step')
+                    elif(log_data["payload"]["log"] == 'position_ramp'):
+                        command_array.append('position_ramp')
+                    elif(log_data["payload"]["log"] == 'speed'):
+                        command_array.append('speed_step')
+                    elif(log_data["payload"]["log"] == 'speed_ramp'):
+                        command_array.append('speed_ramp')
+                    else:
+                        pass
 
-"""
-Creates the base graph for the pendulum experiment.
-"""
-def CreateEmptyPendulumGraph():
-    return 
+                except:
+                    pass
+    
+    return command_array
+
+
+def PositionGraphNodes(G, exp):
+    
+    if(exp == 'spinner'):
+        x = {'voltage_step': 100, 'voltage_ramp': -100, 'position_step': 200, 'position_ramp': -200, 'speed_step': 100, 'speed_ramp': -100 }
+        y = {'voltage_step': -200*math.sin(math.pi/3), 'voltage_ramp': -200*math.sin(math.pi/3), 'position_step': 0, 'position_ramp': 0, 'speed_step': 200*math.sin(math.pi/3), 'speed_ramp': 200*math.sin(math.pi/3)}
+        nx.set_node_attributes(G, x, 'x')
+        nx.set_node_attributes(G, y, 'y')
+
+    return G
--- test/test_process.py
+++ test/test_process.py
@@ -2,18 +2,62 @@ import unittest
 import pandas as pd
 import numpy as np
 import process
-
+import os
 
 
 class TestProcess(unittest.TestCase):
 
-    def test_add_log(self):
-        # process.AddUserLog({"log":"david"})
-        self.assertEqual(0, 0)
+    def test_add_user_log(self):
+        process.AddUserLog({"user": "4321", "exp": "spinner"})
+        try:
+            line = open('./test/data/4321-spinner.json').readline()
+        finally:
+            os.remove('./test/data/4321-spinner.json')
+
+        self.assertEqual(line, '{"user": "4321", "exp": "spinner"}\n')
 
 
+    def test_command_list(self):
+        array = process.GetCommandList('1234', 'spinner')
+        self.assertEqual(len(array), 10)
+
+    def test_generate_matrix(self):
+        process.GenerateAdjacencyMatrix('1234', 'spinner', False)
+
+        try:
+            df = pd.read_csv('./test/data/1234-spinner-adjacency.csv', index_col=0)
+        finally:
+            os.remove('./test/data/1234-spinner-adjacency.csv')
+
+        with self.subTest():
+            self.assertEqual(df['voltage_step']['voltage_step'], 1)
+        with self.subTest():
+            self.assertEqual(df['position_step']['position_step'], 2)
+        with self.subTest():
+            self.assertEqual(df['position_ramp']['position_ramp'], 3)
+
+    def test_generate_matrix_with_existing_matrix(self):
+        process.GenerateAdjacencyMatrix('1234', 'spinner', False)
+        process.GenerateAdjacencyMatrix('1234', 'spinner', False)
+
+        try:
+            df = pd.read_csv('./test/data/1234-spinner-adjacency.csv', index_col=0)
+        finally:
+            os.remove('./test/data/1234-spinner-adjacency.csv')
+
+        with self.subTest():
+            self.assertEqual(df['voltage_step']['voltage_step'], 2)
+        with self.subTest():
+            self.assertEqual(df['position_step']['position_step'], 4)
+        with self.subTest():
+            self.assertEqual(df['position_ramp']['position_ramp'], 6)
 
     
+    # def test_draw_graph(self):
+    #     process.GenerateAdjacencyMatrix('1234', 'spinner', False)
+    #     process.DrawGraph('1234', 'spinner')
+    #     os.remove('./test/data/1234-spinner-adjacency.csv')
+
 
 if __name__ == '__main__':
     unittest.main()
