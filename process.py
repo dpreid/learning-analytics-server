832f7c21ffdd1874d7c6756df1bb20127c580c97
dpreid <dprydereid@gmail.com>
2022-11-18 15:38:54 +0000

    updated affective states

--- process.py
+++ process.py
@@ -57,8 +57,8 @@ def AddUserFeedback(message):
         if(os.path.isfile('%s/%s' % (data_dir, filename))):
             df = pd.read_csv('%s/%s' % (data_dir, filename), index_col=0)
         else:
-            states = ['Engaged', 'Curious', 'Interested', 'Excited', 'Satisfied', 'Delighted', 'Dissatisfied', 'Bored', 'Confused', 'Uninterested', 'Frustrated', 'Surprised']
-            subjects = ['Teaching', 'Workbook', 'Remote work', 'Hardware', 'UI', 'LA']
+            states = ['Engaged', 'Curious', 'Delighted', 'Bored', 'Confused', 'Frustrated', 'Surprised', 'Anxious']
+            subjects = ['Workbook', 'Remote work', 'Hardware', 'UI', 'LA']
             if(course == 'cie3'):
                 subjects.push('spinner-cie3-1-2')
                 subjects.push('spinner-cie3-3')
