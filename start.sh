77bf996742d3a5674834267c001bbb5b6fcd1839
dpreid <dprydereid@gmail.com>
2023-05-09 11:35:27 +0100

    removed docker compose files

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## volume is located in host at /var/lib/docker/volumes/<volume-name>/
-app="analytics"
-volume="user-storage"
-docker build . -t ${app}
-docker volume create ${volume}
-docker run -d -v ${volume}:/home/david/temp/docker ${app}
